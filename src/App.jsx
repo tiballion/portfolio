631030a12b628f49fdc8a403991196526cd97774
tiballion <timothee.ballion.33@gmail.com>
2022-12-02 16:20:42 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <button className="App-button">Click me</button>
+      <p>Test</p>
+    </div>
+  )
+}
+
+export default App
