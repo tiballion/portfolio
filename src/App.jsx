a257ccfd4c0118010d94c575090a98545c9fc4fd
tiballion <timothee.ballion.33@gmail.com>
2022-12-02 21:30:19 +0100

    added github commits calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Intro from './components/Intro'
 import Projects from './components/Projects'
+import Calendar from './components/Calendar'
 
 function App() {
   return (
     <div className="App">
       <Intro />
       <Projects />
+      <Calendar />
     </div>
   )
 }
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+
+import './styles/calendar.css';
+
+function Calendar() {
+    return (
+        <div className="Calendar">
+            <GitHubCalendar username="tiballion" />
+        </div>
+    )
+}
+
+export default Calendar
