45ae1672a4808168564831d076f5c157b59dbbea
tiballion <timothee.ballion.33@gmail.com>
2022-12-05 14:16:42 +0100

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './styles/footer.css';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <p>footer</p>
+        </div>
+    );
+}
+
+export default Footer;
