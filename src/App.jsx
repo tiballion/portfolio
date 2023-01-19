703a014c5bdc565c8f864d03da482f729b80720d
tiballion <timothee.ballion.33@gmail.com>
2023-01-19 15:05:29 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import React, { useState } from "react"
-import "./App.css"
-import Intro from "./components/Intro"
-import Projects from "./components/Projects"
-import Calendar from "./components/Calendar"
-import Footer from "./components/Footer"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
-  const [lang, setLang] = useState("en")
-  const [flag, setFlag] = useState("🇬🇧")
-
-  const switchLang = () => {
-    if (lang === "en") {
-      setLang("fr")
-      setFlag("🇫🇷")
-    } else {
-      setLang("en")
-      setFlag("🇬🇧")
-    }
-  }
-
   return (
-    <>
-      <button className="langbutton" onClick={switchLang}>
-        {flag}
-      </button>
-      <div className="App">
-        <Intro lang={lang} />
-        <Projects lang={lang} />
-        <Calendar />
-        <Footer />
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import "./Home.css"
+import Intro from "../components/Intro"
+import Projects from "../components/Projects"
+import Calendar from "../components/Calendar"
+import Footer from "../components/Footer"
+
+function Home() {
+  const [lang, setLang] = useState("en")
+  const [flag, setFlag] = useState("🇬🇧")
+
+  const switchLang = () => {
+    if (lang === "en") {
+      setLang("fr")
+      setFlag("🇫🇷")
+    } else {
+      setLang("en")
+      setFlag("🇬🇧")
+    }
+  }
+
+  return (
+    <>
+      <button className="langbutton" onClick={switchLang}>
+        {flag}
+      </button>
+      <div className="Home">
+        <Intro lang={lang} />
+        <Projects lang={lang} />
+        <Calendar />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Home
